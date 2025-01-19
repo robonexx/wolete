cd7169766168b04e76e82e6e478bef3b98c2b9a8
robonexx <robertwagar@gmail.com>
2025-01-19 12:32:29 +0100

    fixed text/image section

--- src/components/HomeSectionThree/index.jsx
+++ src/components/HomeSectionThree/index.jsx
@@ -6,19 +6,19 @@ const HomeSectionThree = () => {
     <div className={styles.homeSectionThree}>
       <p className={styles.text}>
         Rooted in the <span>vibrant traditions</span>{' '}
-        <img src='/images/image6.png' alt='' className={styles.highlight} /> of
+        <img src='/images/image7.png' alt='' className={styles.highlight} /> of
         Afrocentric culture and driven by a deep{' '}
         <span>passion for community</span>, <br />
         <img
           src='/images/image8.jpeg'
           alt=''
-          className={styles.highlight}
+          className={styles.highlighttwo}
         />{' '}
         <br />
         her journey is one of{' '}
         <span>connection, rhythm, and transformation</span>. <br />
         Rebecca’s dedication to her craft{' '}
-        <img src='/images/image7.png' alt='' className={styles.highlight} /> has
+        <img src='/images/image6.png' alt='' className={styles.highlightthree} /> has
         led her to perform at renowned institutions like Dramaten, <br />
         live shows with artists both nationally and internationally,
         <br /> experiences that have enriched her perspective and expanded her
