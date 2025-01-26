de7543953dd6c9d848ada356119d1e5609bd80d3
robonexx <robertwagar@gmail.com>
2025-01-26 16:12:29 +0100

    ...

--- src/components/HomeSectionThree/index.jsx
+++ src/components/HomeSectionThree/index.jsx
@@ -5,28 +5,26 @@ const HomeSectionThree = () => {
   return (
     <div className={styles.homeSectionThree}>
       <p className={styles.text}>
-        Rooted in the <span>vibrant traditions</span>{' '}
-        <img src='/images/image7.png' alt='' className={styles.highlight} /> of
-        Afrocentric culture and driven by a deep{' '}
-        <span>passion for community</span>, <br />
+        Rooted{' '}
+        <img src='/images/image7.png' alt='' className={styles.highlight} /> in
+        the <span>vibrant traditions</span> of Afrocentric culture and driven by
+        a deep <span>passion for community</span>
         <img
-          src='/images/image8.jpeg'
+          src='/images/wolete.JPG'
           alt=''
           className={styles.highlighttwo}
         />{' '}
-        <br />
         her journey is one of{' '}
         <span>connection, rhythm, and transformation</span>. <br />
-        Rebecca’s dedication to her craft{' '}
+        Rebecca’s dedication to her craft has led her to perform at renowned
+        institutions like <span>Dramaten</span>, live shows with artists both{' '}
+        <span>nationally</span>
         <img
-          src='/images/image6.png'
+          src='/images/gallery/ROR1.jpg'
           alt=''
           className={styles.highlightthree}
         />{' '}
-        has led her to perform at renowned institutions like{' '}
-        <span>Dramaten</span>, <br />
-        live shows with artists both <span>nationally and internationally</span>
-        ,
+        <span>and internationally</span>
         <br /> experiences that have enriched her perspective and{' '}
         <span>expanded</span> her&nbsp;
         <span>creative horizons</span>.
